6d2b301abdbe14257f15dbb6a2c18fc01f610cbd
Jennifer Machado <dev.jenni@outlook.com>
2024-01-18 12:11:01 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Web-Components-no-Front-end
+
+Treinamento em Web Components para HTML e CSS, no qual preservei a estrutura original em HTML/CSS e introduzi dinamicamente elementos em JavaScript para criar uma SHADOW DOM, conferindo um formato de blog de notícias à interface.
+
+
+## 📱 Funcionalidades: 
+- Web components usando shadow DOM
+- Maior capacidade de reutilização de código
+- Facilitando escrita de HTML e CSS, tornando-o dinamico
+
+
+## 🤳 Demonstração:
+- Você pode clicar no link abaixo:
+
+  [WebComponents]()
+
+## 👩🏻‍💻 Imagem do projeto:
+
+
+!![Imagem](/assets/img/projeto.png)
+
+
+## 🤖 Tecnologias Utilizadas:
+
+- NodeJs Versão LTS Mais Recente: 18.18.0 (includes npm 9.8.1)
+- Package Http-server
+- Css
+- HTML
+- Java Script
+
---
+++ assets/root-app.js
@@ -0,0 +1,91 @@
+class CardNews extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: "open" })
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.style())
+    }
+
+    build() {
+        const componentRoot = document.createElement('div')
+        componentRoot.setAttribute('class', 'card')
+
+        const cardLeft = document.createElement('div')
+        cardLeft.setAttribute('class', 'card-left')
+
+        const autor = document.createElement('span')
+        autor.textContent = 'By ' + (this.getAttribute('autor') || 'Anonymous')
+
+        const title = document.createElement('h1')
+        title.textContent = this.getAttribute('title')
+
+        const linkUrl = document.createElement('a')
+        linkUrl.textContent = this.getAttribute('link-url') || ''
+        linkUrl.href = this.getAttribute('link-url')
+
+        const newsContent = document.createElement('p')
+        newsContent.textContent = this.getAttribute('newsContent')
+
+        cardLeft.appendChild(autor)
+        cardLeft.appendChild(linkUrl)
+        cardLeft.appendChild(title)
+        cardLeft.appendChild(newsContent)
+
+
+        const cardRight = document.createElement('div')
+        cardRight.setAttribute('class', 'card-right')
+
+        const newsImage = document.createElement('img')
+        newsImage.src = this.getAttribute('photo') || "/assets/img/default-image.jpg"
+        cardRight.appendChild(newsImage)
+
+        componentRoot.appendChild(cardLeft)
+        componentRoot.appendChild(cardRight)
+
+        return componentRoot
+    }
+
+
+    style() {
+        const style = document.createElement('style')
+        style.textContent = `
+        .card {
+            width: 80%;
+            box-shadow: 12px 11px 13px -5px rgba(0,0,0,0.55);
+            -webkit-box-shadow: 12px 11px 13px -5px rgba(0,0,0,0.55);
+            -moz-box-shadow: 12px 11px 13px -5px rgba(0,0,0,0.55);
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        
+        .card-left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        
+        .card-left>span{
+            font-weight: 400;
+        }
+        
+        .card-left>h1 {
+            margin-top: 15px;
+            font-size: 25px;
+        }
+        
+        .card-left>p {
+            color: rgb(20, 20, 20);
+        } 
+
+        .card-right  img{
+            max-width: 350px;
+            max-height: 330px;
+        }
+        `
+        return style
+    }
+}
+
+customElements.define('card-news', CardNews)
